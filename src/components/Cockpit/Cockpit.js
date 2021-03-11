aa3c20d47e88e526afbab8a611ff6b166df40923
ChingizPro <chingizprog@gmail.com>
2021-03-11 18:04:32 +0500

    Create <<Wrapper.js>> file and use it as adjacent JSX elements wrapper

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import classes from './Cockpit.module.css';
+import Wrapper from '../../hoc/Wrapper';
 
 const Cockpit = props => {
     useEffect(() => {
@@ -33,12 +34,14 @@ const Cockpit = props => {
     let toggleClasses = [classes.toggler];
     if (props.visible) toggleClasses.push(classes.red);
 
-    return [
-        <h1 key='el1' style={{ color: 'aqua' }}>A React App created with Components</h1>,
-        <p key='el2' className={textClasses.join(' ')} style={{ fontSize: 18 }}>Just some text, nothing more</p>,
-        <div key='el3' style={{ marginBottom: 10 }} onClick={props.resetPersons}><button className={classes.btn}>Reset</button></div>,
-        <div key='el4'><button onClick={props.togglePersons} className={toggleClasses.join(' ')}>Toggle Persons</button></div>
-    ];
+    return (
+        <Wrapper>
+            <h1 key='el1' style={{ color: 'aqua' }}>A React App created with Components</h1>
+            <p key='el2' className={textClasses.join(' ')} style={{ fontSize: 18 }}>Just some text, nothing more</p>
+            <div key='el3' style={{ marginBottom: 10 }} onClick={props.resetPersons}><button className={classes.btn}>Reset</button></div>
+            <div key='el4'><button onClick={props.togglePersons} className={toggleClasses.join(' ')}>Toggle Persons</button></div>
+        </Wrapper>
+    );
 }
 
 export default React.memo(Cockpit);
